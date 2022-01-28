88a823706e0892b8753afe2adff86aa19a9ef518
Arantxa Morcillo <arantxa.morcillo@gmail.com>
2022-01-28 10:51:00 +0100

    exercise done

--- src/main/java/com/BS3/CiclodevidadeBeans/CicloDeVidaDeBeansApplication.java
+++ src/main/java/com/BS3/CiclodevidadeBeans/CicloDeVidaDeBeansApplication.java
@@ -1,8 +1,10 @@
 package com.BS3.CiclodevidadeBeans;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
 
@@ -15,6 +17,22 @@ public class CicloDeVidaDeBeansApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(CicloDeVidaDeBeansApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner ejecutame(){
+		return p ->
+		{
+			System.out.println("Hello second class");
+		};
+	}
+
+	@Bean
+	CommandLineRunner ejecutame2(){
+		return p ->
+		{
+			System.out.println("It's Third class here!");
+		};
 	}
 
 	@PostConstruct
